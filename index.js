5acc3c9f97e1401bd133bdc3aadd18fe5325ca43
Sixtey7 <sixtey7@gmail.com>
2017-01-18 21:24:03 -0500

    should not check in build js

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(methodOverride());
